32edc6449407ec819f43c754bda30214574c19b3
golbang2 <karigan05@naver.com>
2020-09-03 00:13:25 +0900

    back1.0

--- kospi_portfolio_reinforcement_learning/environment.py
+++ kospi_portfolio_reinforcement_learning/environment.py
@@ -30,10 +30,9 @@ class env:
         self.state_prime = self.env_array[:, self.time : self.time + self.day_len,:]
         self.y = self.state_prime[:,-1,0]/self.state[:,-1,0]
         self.value = self.value*np.sum(weight*self.y)
-        self.r = (np.sum(weight*self.y)-1)*100
-        self.r = np.stack((self.r,)*self.state.shape[0],axis=-1)
+        self.r = np.expand_dims((self.y-1)*100,axis=0)
         if self.time == self.env_array.shape[1]-self.day_len:
             self.done = True
         return self.state_prime, self.r, self.done, self.value
-
+    
 w = np.array([0.2,0.1,0.05,0.1,0.05,0.3,0.03,0.02,0.05,0.1])
--- kospi_portfolio_reinforcement_learning/main.py
+++ kospi_portfolio_reinforcement_learning/main.py
@@ -37,7 +37,7 @@ def MM_scaler(s):
     
 
 #preprocessed data loading
-is_train = 'train'
+is_train = 'test'
 path_data = './preprocess/'+is_train+'_price_tensor.npy'
 asset_data = np.load(path_data, allow_pickle=True)
 
@@ -48,18 +48,17 @@ input_day_size = 50
 filter_size = 3
 num_of_feature = asset_data.shape[2]
 num_of_asset = asset_data.shape[0]
-num_episodes = 3001
+num_episodes = 30000 if is_train =='train' else 1
 
 #saving
 save_frequency = 100
 save_path = './algorithms'
+save_model = 1
+load_model = 1
 if is_train=='test':
     env = environment.env(train = False)
 else:
     env = environment.env()
-save_model = 1
-load_model = 0
-env = environment.env()
 
 config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
 config.gpu_options.allow_growth = True
@@ -73,25 +72,25 @@ with tf.Session(config=config) as sess:
         ckpt = tf.train.get_checkpoint_state(save_path)
         if load_model:
             saver.restore(sess,ckpt.model_checkpoint_path)
-
+    
     for i in range(num_episodes):
-        episode_memory = []
+        episode_memory = deque()
         s=env.start()
         s=MM_scaler(s)
         done=False
-        m = np.zeros([10,1,20],dtype=np.float32)/10
+        m = np.zeros([10,1,20],dtype=np.float32)
         while not done:
             w= agent.predict(s,m)
             s_prime,r,done,value = env.action(w)
             s_prime=MM_scaler(s_prime)
-            agent.buffer(s,r,m)
+            episode_memory.append([s,r,m])
             m=memory_queue(m,w)
             s = s_prime
             if done:
-                agent.update(episode_memory)
                 print(i,value)
                 if is_train =='train':
-                    agent.update()
-        if save_model ==1 and i%save_frequency == 99:
+                    agent.update(episode_memory)
+                
+        if save_model == 1 and i % save_frequency == save_frequency - 1:
             saver.save(sess,save_path+'/model-'+str(i)+'.cptk')
             print('saved')
